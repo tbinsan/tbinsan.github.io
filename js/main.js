5497bc55d0c20a0a89c6fb9949f7f27c30ffd59f
Yerbol Syzdyk <erbcreedok@gmail.com>
2018-01-14 02:26:26 +0600

    phone changed, numbers deleted

--- js/main.js
+++ js/main.js
@@ -1,31 +1,3 @@
-;(function() {
-    var countersAnimate = function() {
-        var counters = $('#counters');
-        if ( counters.length > 0 ) {
-
-            counters.waypoint( function( direction ) {
-
-                if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-
-                    setTimeout(function() {
-                        counters.find('.js-counter').countTo({
-                            formatter: function (value, options) {
-                                return value.toFixed(options.decimals);
-                            },
-                        });
-                    }, 400);
-
-                    $(this.element).addClass('animated');
-
-                }
-            } , { offset: '80%' } );
-
-        }
-    };
-
-    countersAnimate();
-})();
-
 function toggleClass(element, className) {
     $(element).toggleClass(className);
 }
